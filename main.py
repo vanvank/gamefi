f9dcecdb513a76f177b013be649c671849bc916a
vanvank <165584943@qq.com>
2021-11-11 14:12:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,55 +11,39 @@ def search_pic_click(pic_path):
     #截屏，同时提前准备一张屏幕上会出现的小图bd.png
     im=ImageGrab.grab()
     im.save('screen.png','png')
-
     #加载原始RGB图像
     img_rgb= cv2.imread("screen.png")
-
     #创建一个原始图像的灰度版本，所有操作在灰度版本中处理，然后在RGB图像中使用相同坐标还原
     img_gray=cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-
     #加载将要搜索的图像模板
     template= cv2.imread(pic_path, 0)
-
     #使用matchTemplate对原始灰度图像和图像模板进行匹配
     res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-
     #设定阈值,0.7应该可以
     threshold= 0.85 #res大于99.9%#
     loc = np.where(res >= threshold)
-    #print(type(loc[0]))
-    #if str(loc[0]) == "":
-     #   print("True")
-
     #得到原图像中的坐标
     for pt in zip(*loc[::-1]):
         print(pt[0], pt[1])
         pyautogui.click(pt[0],pt[1])
         break
-        #cv2.destroyAllWindows()
+    #cv2.destroyAllWindows()
 
 
 def task_pvp():
-    print("pvp")
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " ---- pvp")
     search_pic_click(pvp_button)
-    time.sleep(0.2)
+    time.sleep(0.5)
     search_pic_click(ok_button)
-    time.sleep(0.2)
-    search_pic_click(pvp_button)
-    time.sleep(0.2)
-    search_pic_click(ok_button)
-    time.sleep(0.2)
+    time.sleep(0.5)
 
 
 def task_pve():
-    print("pve")
+    print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+ " ---- pve" )
     search_pic_click(ok_button)
-    time.sleep(0.2)
+    time.sleep(0.5)
     search_pic_click(challeng_button)
-    time.sleep(0.2)
-    search_pic_click(ok_button)
-    time.sleep(0.2)
-    search_pic_click(challeng_button)
+    time.sleep(0.5)
 
 
 # task_tpye=1，只跑pvp； task_type=2，只跑pve； task_type=3，两种任务都跑
@@ -74,12 +58,13 @@ def run(task_type,last_time=1800):
             task_pve()
         if task_type == 3:
             task_pve()
-            task_pve()
+            task_pvp()
         time.sleep(2)
         if time.time() - now > last_time:
             print('已过%s秒' % last_time)
             print(start_time)
             print(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+            cv2.destroyAllWindows()
             break
 
 
@@ -88,8 +73,4 @@ if __name__=="__main__":
     pvp_button = r'C:\Users\wan\Desktop\Yohero\pvp.png'
     ok_button = r'C:\Users\wan\Desktop\Yohero\ok_button.png'
     challeng_button = r'C:\Users\wan\Desktop\Yohero\challeng.png'
-    run(1)
-
-
-
-
+    run(2)
